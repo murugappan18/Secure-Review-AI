87ca54ce698818594305018b603b7def4ba0d934
Murugappan P <117421163+murugappan18@users.noreply.github.com>
2026-06-20 13:36:41 +0530

    Updated Portfolio Link

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -33,7 +33,7 @@ function Linkedin({ className }) {
 
 const LINKS = {
   github: 'https://github.com/murugappan18',
-  portfolio: 'https://murugappan18.github.io/my-portfolio',
+  portfolio: 'https://www.murugappanthedev.me',
   linkedin: 'https://www.linkedin.com/in/murugappan-p',
   email: 'mailto:murugappanp24@gmail.com'
 };
